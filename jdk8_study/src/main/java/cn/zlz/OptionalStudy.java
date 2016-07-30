afc65ff0f7e643203a1f0131c7682796cb04b73c
lizhi <714131254@qq.com>
2016-07-30 15:42:04 +0800

    ..

---
+++ jdk8_study/src/main/java/cn/zlz/OptionalStudy.java
@@ -0,0 +1,10 @@
+package cn.zlz;
+
+/**
+ * Created by lizhi on 2016/7/30.
+ */
+public class OptionalStudy {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
